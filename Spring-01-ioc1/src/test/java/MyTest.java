2c8dbf9ec2c735c19f517f70eef793a5b02af9cb
julin <418736745@qq.com>
2022-07-12 14:23:59 +0800

    111

--- Spring-01-ioc1/src/test/java/MyTest.java
+++ Spring-01-ioc1/src/test/java/MyTest.java
@@ -3,14 +3,22 @@ import com.julin.dao.UserDaoOracleImpl;
 import com.julin.service.UserService;
 import com.julin.service.UserServiceImpl;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
 
     @Test
     public void getUser(){
-        //用户实际调用的是业务层，dao实际上不接触
+    /*    //用户实际调用的是业务层，dao实际上不接触
         UserService userService = new UserServiceImpl();
         ((UserServiceImpl)userService).setUserDao(new UserDaoOracleImpl());
-        userService.getUser();
+        userService.getUser();*/
+
+        //获取ApplicationContext 拿到Spring的容器
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        //需要什么就get什么
+        UserServiceImpl userServiceImpl = (UserServiceImpl) context.getBean("UserServiceImpl");
+        userServiceImpl.getUser();
+
     }
 }
