e9f8f952ef1fe5c9ccb60d8651d7e46e2393e6bb
julin <418736745@qq.com>
2022-07-14 00:55:44 +0800

    111

---
+++ Spring-04-di/src/main/java/com/julin/pojo/Address.java
@@ -0,0 +1,12 @@
+package com.julin.pojo;
+
+public class Address {
+    private final String address = "北下关";
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "address='" + address + '\'' +
+                '}';
+    }
+}
---
+++ Spring-04-di/src/main/java/com/julin/pojo/User.java
@@ -0,0 +1,15 @@
+package com.julin.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String name;
+    private int age;
+
+
+}
