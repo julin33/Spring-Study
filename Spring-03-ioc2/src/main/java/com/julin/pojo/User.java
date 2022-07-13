d7763c69a1856aa9bbeeaf22d3304be36ee36c34
julin <418736745@qq.com>
2022-07-13 19:06:49 +0800

    111

--- Spring-03-ioc2/src/main/java/com/julin/pojo/User.java
+++ Spring-03-ioc2/src/main/java/com/julin/pojo/User.java
@@ -6,8 +6,8 @@ import lombok.Data;
 public class User {
     private String name;
 
-    public User (){
-        System.out.println("User的无参构造");
+    public User (String name){
+        this.name = name;
     }
     public void show(){
         System.out.println("name=" + name);
